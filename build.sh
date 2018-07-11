59c4f3a17ba60c9fed646d6be22b893580a3caba
Kevin Woldt <bug@230woldt.de>
2018-07-11 21:57:31 +0200

    Only tag latest if nothing is given

--- build.sh
+++ build.sh
@@ -14,22 +14,24 @@ TAG.
 HEREDOC
 }
 
-usage
 while [ $# -gt 0 ]; do
 	arg="$1"
 	shift
 	name=$(echo "$arg" | cut -d: -f1)
-	if [ -z "$name" ]; then
+	if [ "$name" = "-h" ] || [ "$name" = "--help" ]; then
+		usage
+		exit 0
+	elif [ -z "$name" ]; then
 		error "$name is not a valid tag"
 		usage
 		exit 1
 	fi
 	tags=$(echo "$arg" | cut -d: -f2- | tr ':' '\n')
 	if [ "$name" = "$tags" ]; then
-		tags=''
+		tags='latest'
 	fi
 	if [ -f "$name/Dockerfile" ]; then
-		img_id=$(cd "$name"; LANG=C LC_TYPE=C docker build -t "kwoldt/$name" . | tee /dev/stderr | awk '/Successfully built/{print $NF}')
+		img_id=$(cd "$name"; LANG=C LC_TYPE=C docker build . | tee /dev/stderr | awk '/Successfully built/{print $NF}')
 		for tag in $tags; do
 			docker tag "$img_id" "kwoldt/$name:$tag"
 		done
